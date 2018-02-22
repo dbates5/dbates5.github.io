743fec9080ede611005466e5017575e0f2bb6505
Dylan <dbates5@lsu.edu>
2018-02-22 11:01:15 -0600

    code 4

--- code-assignment-04/code-assignment-04.js
+++ code-assignment-04/code-assignment-04.js
@@ -1,1 +1,34 @@
-
+let projectArray = [
+  1,
+  'https://dbates5.github.io',
+  'Dylans Portfolio',
+  'Weekly updates',
+  true
+]
+let projectObject = {
+  id: 1,
+  url: 'https://dbates5.maps.arcgis.com/apps/GeoForm/index.html?appid=cffc99a317ff484abfb27ee1a5acacdf',
+  title: 'School Locations',
+  desc: 'All locations of East Baton Rouge Schools',
+  thumb: true,
+}
+let projectArray = [
+  1,
+  'https://dbates5.github.io',
+  'Dylans Portfolio',
+  'new updates weekly',
+  true,
+  ['Project', 'Here and now', 'New']
+]
+let projectObject = {
+  id: 1,
+  url: 'https://dbates5.maps.arcgis.com/apps/webappviewer/index.html?id=5fb4d2f9bc82467c87ee00d8332dacd3',
+  title: 'City Locations',
+  desc: 'Locations of cities from 1790 to 2000',
+  thumb: true,
+  keywords: [
+    'City', 
+    'Locations and coordinates',
+    'Locations'
+  ]
+}
