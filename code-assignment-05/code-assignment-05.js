7efb958d7864d2625fa2d9c92dda1a9d5d5d797a
Dylan <dbates5@lsu.edu>
2018-02-26 15:41:04 -0600

    Code 5 change

--- code-assignment-05/code-assignment-05.js
+++ code-assignment-05/code-assignment-05.js
@@ -3,20 +3,20 @@ let project1 = {
   title: 'Dangerous Dogs',
   hasThumbnail: true
 }
+
 let project2 = {
   id: 2,
   title: 'US Cities',
   hasThumbnail: true
 }
+
 let projects = [
   project1,
   project2
 ]
-for (let i = 0; i < projects.length; i++) {
-project1, project2
-}
-console.log('Element ' + i + ' title: ' + projects[i].title)
-console.log('images/ss-project-' + projects[i].id + '.png')
 
-if (projects[i].hasThumbnail === true) {
-  console.log('images/ss-project-' + projects[i].id + '.png') }
+for (let i = 0; i < projects.length; i++) {
+  console.log('Element ' + i + ' title: ' + projects[i].title)
+  console.log('images/ss-project-' + projects[i].id + '.png')
+  if (projects[i].hasThumbnail === true) {
+    console.log('images/ss-project-' + projects[i].id + '.png') } }
