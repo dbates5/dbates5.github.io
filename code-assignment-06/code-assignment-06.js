36593a74fb2c2b8d7227a3a84b4871bb927378d3
Dylan <dbates5@lsu.edu>
2018-04-03 10:06:04 -0500

    d

--- code-assignment-06/code-assignment-06.js
+++ code-assignment-06/code-assignment-06.js
@@ -1,4 +1,2 @@
-let grapefruit = 'The ' + grapefruit + ' is a delicious snack.'
-let myfavfruit = 'grapefruit'
-let grapefruit2 = describeFruit(myfavfruit)
-console.log(grapefruit)
+let coolfruit = 'The ' + grapefruit + ' is a delicious snack.'
+console.log(describeFruit('grapefruit'))
